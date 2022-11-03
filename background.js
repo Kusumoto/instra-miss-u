7d1cc61d21fae3483348c2c78d11f53df4142491
Weerayut Hongsa <kusumoto.com@gmail.com>
2022-11-03 10:00:25 +0700

    Update background.js

--- background.js
+++ background.js
@@ -9,6 +9,16 @@ chrome.declarativeNetRequest.updateDynamicRules({
             'urlFilter': 'i.instagram.com/api/v1/stories/reel/seen',
             'resourceTypes': ['xmlhttprequest']
         }
+    },{
+        'id': 1002,
+        'priority': 1,
+        'action': {
+            'type': 'block'
+        },
+        'condition': {
+            'urlFilter': 'www.instagram.com/api/v1/stories/reel/seen',
+            'resourceTypes': ['xmlhttprequest']
+        }
     }],
-    removeRuleIds: [1001]
+    removeRuleIds: [1001, 1002]
 })
